ce4d097a45551142ac211d3365f3c577df3bbf4b
Chirayu Desai <chirayudesai1@gmail.com>
2021-04-15 02:09:34 +0530

    msm8998-common: rro_overlays: Move CarrierConfig to RRO
    Change-Id: Ia57acd244608848f70fdcc30d5694c919db60671

--- msm8998.mk
+++ msm8998.mk
@@ -358,6 +358,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.1 \
     android.hardware.radio@1.1 \
+    CarrierConfigOverlay \
     librmnetctl
 
 # Seccomp policy
