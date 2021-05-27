e9a2a6ddd18ec8726a67d1107bb8510cf77f5a85
deny2311 <77781846+deny2311@users.noreply.github.com>
2021-05-27 15:05:21 +0700

    msm8998-common: Add QTI health implementation

--- msm8998.mk
+++ msm8998.mk
@@ -229,7 +229,7 @@ PRODUCT_COPY_FILES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
+    android.hardware.health@2.1-impl-qti \
     android.hardware.health@2.0-service
 
 # HIDL
