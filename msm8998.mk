b14c0179f941c45e46b52d29c90bf0f503d25dc6
Josh Wu <joshwu@google.com>
2022-01-24 22:56:14 -0800

    msm8998-common: Switch BtAudio to AIDL
    Test: m
    Bug: 203490261
    Change-Id: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7
    Merged-In: I4b8ac40d5ad410b89eb6a0573d740d86471ecfc7

--- msm8998.mk
+++ msm8998.mk
@@ -110,7 +110,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     audio.bluetooth.default \
-    android.hardware.bluetooth.audio@2.1-impl \
+    android.hardware.bluetooth.audio-impl \
     libbthost_if
 
 PRODUCT_COPY_FILES += \
