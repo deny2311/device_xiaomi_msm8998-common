08cac5a55a2c7796ef80703c238bcd8b9d8d278c
deny2311 <deny2311@gmail.com>
2022-04-13 12:55:10 +0700

    msm8998-common: Move aux camera packagelist prop to system props

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -111,7 +111,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.camera.set.afd=4 \
     vidc.enc.dcvs.extra-buff-count=2 \
     camera.shutter_sound.blacklist=com.android.camera \
-    vendor.camera.aux.packagelist=com.android.camera,org.lineageos.snap \
     vendor.camera.hal1.packagelist=com.android.camera \
     persist.camera.privapp.list=com.android.camera,com.google.android.GoogleCamera,org.lineageos.snap
 
