705afe209b4b0d133f1a3a457c324f2ac1f4ef74
Nolen Johnson <johnsonnolen@gmail.com>
2022-05-19 12:25:04 -0400

    msm8998-common: Remove deprecated TARGET_USES_QTI_CAMERA_DEVICE
    * Dead as of https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/331507.
    * We never needed this.
    
    Change-Id: I8ec420d6264fee70a1602eecdbb4f34f2e5db3dc

--- msm8998.mk
+++ msm8998.mk
@@ -128,9 +128,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml
 
-PRODUCT_COPY_FILES += \
-    external/ant-wireless/antradio-library/com.dsi.ant.antradio_library.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.dsi.ant.antradio_library.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
