12523d708a361bb238893ba655a1ac67da844a98
deny2311 <deny2311@gmail.com>
2022-07-11 03:01:29 +0000

    msm8998-common: enable zygote preforking

--- system_prop.mk
+++ system_prop.mk
@@ -58,3 +58,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1
+
+# Zygote preforking
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
